3abe77ff62d56f7239273dad4e09eaf6b5365e6a
adillari1493 <adil.lari1493@gmail.com>
2024-07-18 14:22:03 +0530

    modified Java file

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!!123456");
+        System.out.println("Hello!!");
     }
 }
